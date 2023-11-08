6a29e508600ba03d9a3bd2949c0a516b6468989b
Sapdotten <semenova.a.p.04@gmail.com>
2023-11-08 12:10:56 +0400

    test commit

--- include/linked_list/image.cpp
+++ include/linked_list/image.cpp
@@ -1,62 +1,54 @@
 #ifndef INCLUDE_IMAGE_CPP
 #define INCLUDE_IMAGE_CPP
-#include <string>
-#include <linked_list.cpp>
-#include <iostream>
 #include <fstream>
+#include <iostream>
+#include <linked_list.cpp>
+#include <string>
 namespace wolfram_code {
 
- class Image {
+class Image {
   int width_;
   int height_;
   std::string file_name_;
 
  public:
   Image(std::string const& file_name, int width, int height);
-  void AddLine(LinkedList<int> const &list);
- };
+  void AddLine(LinkedList<int> const& list);
+};
 
- Image::Image(std::string const& file_name, int width, int height) { 
+Image::Image(std::string const& file_name, int width, int height) {
   this->file_name_ = file_name;
-  this->width_ = width; 
+  this->width_ = width;
   this->height_ = height;
-  std::ofstream out;      // поток для записи
+  std::ofstream out;     // поток для записи
   out.open(file_name_);  // открываем файл для записи
   if (out.is_open()) {
     out << "P3" << std::endl
-        << this->width_ << " "<<this->height_<<std::endl
+        << this->width_ << " " << this->height_ << std::endl
         << "1" << std::endl;
   }
   out.close();
+}
+
+void Image::AddLine(LinkedList<int> const& list) {
+  std::ofstream out(this->file_name_, std::ios::app);
+  if (out.is_open()) {
+    Node<int>* ptr = list.GetHead();
+    for (int i = 0; i < list.GetSize() - 1; ++i) {
+      int value = ptr->GetValue();
+      out << value << " " << value << " " << value << "    ";
+      ptr = ptr->GetNext();
+    }
+    int value = ptr->GetValue();
+    out << value << " " << value << " " << value << std::endl;
   }
+  out.close();
+}
 
- void Image::AddLine(LinkedList<int> const& list) { 
-   std::ofstream out(this->file_name_, std::ios::app);
-   if (out.is_open()) {
-     Node<int>* ptr = list.GetHead();
-     for (int i = 0; i < list.GetSize()-1; ++i) {
-       int value = ptr->GetValue();
-       out << value << " " << value << " " << value << "    "; 
-       ptr = ptr->GetNext();
-     }
-     int value = ptr->GetValue();
-     out << value << " " << value << " " << value << std::endl; 
-   }
-   out.close();
- }
+// void Image::AddLine(LinkedList<int> const& list) {
 
- //void Image::AddLine(LinkedList<int> const& list) {
+//}
 
- //}
-
-
-
-
-
-
-}// namespace wolfram_code
-
-
-
+}  // namespace wolfram_code
 
 #endif
