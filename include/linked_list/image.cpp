385f105ce1b84db236facf6cf61fa85421c260b2
[Sapdotten] <[semenova.a.p.04@gmail.com]>
2023-11-08 11:56:14 +0400

    realize a minimum-functional class for write down the  wolfram code to file

---
+++ include/linked_list/image.cpp
@@ -0,0 +1,63 @@
+#ifndef INCLUDE_IMAGE_CPP
+#define INCLUDE_IMAGE_CPP
+#include <string>
+#include <linked_list.cpp>
+#include <iostream>
+#include <fstream>
+namespace wolfram_code {
+
+ class Image {
+  int width_;
+  int height_;
+  std::string file_name_;
+
+ public:
+  Image(std::string const& file_name, int width, int height);
+  void AddLine(LinkedList<int> const &list);
+ };
+
+ Image::Image(std::string const& file_name, int width, int height) { 
+  this->file_name_ = file_name;
+  this->width_ = width; 
+  this->height_ = height;
+  std::ofstream out;      // поток для записи
+  out.open(file_name_);  // открываем файл для записи
+  if (out.is_open()) {
+    out << "P3" << std::endl
+        << this->width_ << " "<<this->height_<<std::endl
+        << "1" << std::endl;
+  }
+  out.close();
+  std::cout << "file has been writed ";
+  }
+
+ void Image::AddLine(LinkedList<int> const& list) { 
+   std::ofstream out(this->file_name_, std::ios::app);
+   if (out.is_open()) {
+     Node<int>* ptr = list.GetHead();
+     for (int i = 0; i < list.GetSize()-1; ++i) {
+       int value = ptr->GetValue();
+       out << value << " " << value << " " << value << "    "; 
+       ptr = ptr->GetNext();
+     }
+     int value = ptr->GetValue();
+     out << value << " " << value << " " << value << std::endl; 
+   }
+   out.close();
+ }
+
+ //void Image::AddLine(LinkedList<int> const& list) {
+
+ //}
+
+
+
+
+
+
+}// namespace wolfram_code
+
+
+
+
+#endif
